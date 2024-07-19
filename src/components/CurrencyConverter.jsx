e178e26fe9e0a8807e8d9840cf799e884e4354b4
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-07-19 12:39:27 +0000

    added api

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,104 +1,115 @@
 import "../styles/CurrencyConverter.css"
 import { RequestPickup } from "./RequestPickup.jsx"
-import { useState } from "react"
-import { Router, Navigate, useNavigate, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Router } from "react-router-dom"
+
+const toField = document.querySelector('.to-field')
+const fromField = document.querySelector('.from-field')
+
+const selectFromEl = document.querySelector('.from-select')
+const fromUSD = document.querySelector('#USD')
+
 
 export const CurrencyConverter = () => {
-    const navigate = useNavigate()
     // API
-    const fetchRates = async() => {
+    const convertCurrency = async() => {
         try {
-            const data = await fetch('https://data.fixer.io/api/latest')
-            const parsedData = data.json()
-            console.log(data)
+            const data = await fetch('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_clVUQQecPGeX7P34kbBEfvEjGjrdVUpXvRhsR86c')
+            const parsedData = await data.json()
+
+            for (let eachCurrency in parsedData) {
+                // an object of conversion rates from USD to other currencies
+                let ratesObject = parsedData[eachCurrency]
+                // get the key of ratesObject
+                let keys = Object.keys(ratesObject)
+                let values = Object.values(ratesObject)
+
+                // 1 - USD -> EUR
+                // take number from to-field and multiply with eRate of eur
+
+            }
+
         } catch (error) {
             console.error(error)
         }
 
     }
 
+    const handleChange = (event) => {
+        // inner value of <option> element
+        console.log(event.target.value)
+    }
+
     const [clicked, setClicked] = useState(false)
     const redirectClick = () => {
-        // setClicked(true)
-        navigate('/request-pickup')
+        setClicked(true)
     }
 
   return (
     <>
-        <Router>
-            <h1 className='currencytitle'>Currency Converter</h1>
-    
-            <div className="currency-component-wrapper">
-                <div className='converter-container'>
-                    <div className="from-container">
-                        <h3>From:</h3>
+        <h1 className='currencytitle'>Currency Converter</h1>
 
-                        <div className="actual-converter">
-                            <input type="number" className='number-field' />
-                            <select name="" >
-                                <option value="USD">USD</option>
-                                <option value="GHS">GHS</option>
-                                <option value="EUR">EUR</option>
-                                <option value="GBP">GBP</option>
-                                <option value="USD">USD</option>
-                            </select>
-                        </div>
+        <div className="currency-component-wrapper">
+            <div className='converter-container'>
+                <div className="from-container">
+                    <h3>From:</h3>
 
-                        <div className="rate-container">
-                            <div className="rate">
-                                <h6> 1 USD = 15.21 GHS</h6>
-                            </div>
-                        </div>
+                    <div className="actual-converter">
+                        <input type="number" className='number-field' id="from-field" />
+                        <select name="" className="from-select" onChange={handleChange} >
+                            <option value="USD" id="USD">USD</option>
+                            <option value="GHS" id="GHS">GHS</option>
+                            <option value="EUR" id="EUR">EUR</option>
+                            <option value="GBP" id="EUR">EUR</option>
+                        </select>
                     </div>
 
-                    <div className="to-container">
-                        <h3>To:</h3>
-
-                        <div className="actual-converter">
-                            <input type="number" className='number-field' disabled />
-                            <select name="" >
-                                <option value="USD">USD</option>
-                                <option value="GHS">GHS</option>
-                                <option value="EUR">EUR</option>
-                                <option value="GBP">GBP</option>
-                                <option value="USD">USD</option>
-                            </select>
-                        </div>
-
-                        <div className="rate-container">
-                            <div className="rate">
-                                <h6> 1 GHS = 0.0657 USD</h6>
-                            </div>
+                    <div className="rate-container">
+                        <div className="rate">
+                            <h6> 1 USD = 15.21 GHS</h6>
                         </div>
                     </div>
-
-                    <div className="btn-container">
-                        <button className='convert-btn' onClick={fetchRates} >Convert</button>
-                    </div>
-
-                    
                 </div>
+
+                <div className="to-container">
+                    <h3>To:</h3>
+
+                    <div className="actual-converter">
+                        <input type="number" className='number-field' id="to-field" disabled/>
+                        <select name="" className="to-select">
+                            <option value="USD" id="USD">USD</option>
+                            <option value="GHS" id="GHS">GHS</option>
+                            <option value="EUR" id="EUR">EUR</option>
+                            <option value="GBP" id="EUR">EUR</option>
+                        </select>
+                    </div>
+
+                    <div className="rate-container">
+                        <div className="rate">
+                            <h6> 1 GHS = 0.0657 USD</h6>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="btn-container">
+                    <button className='convert-btn' onClick={convertCurrency} >Convert</button>
+                </div>
+
+                
             </div>
+        </div>
 
 
-            
-            <div className="request-container">
-                {/* Redirect to RequestPickup.jsx when clicked */}
-                <button className='request-pickup-btn' onClick={redirectClick}>
-                    Request pickup
-                </button>
-            
-                {/* route for pickup request */}
-                <Routes>
-                    {/* <Route path="/redirect"
-                     element={clicked ? <Navigate to="/request-pickup"/> : <p>no pickup request</p> }
-                     /> */}
+        
+        <div className="request-container">
+            {/* Redirect to RequestPickup.jsx when clicked */}
+            <button className='request-pickup-btn'>
+                Request pickup
+            </button>
+        
+            {/* route for pickup request */}
 
-                     <Route path="/request-pickup" element={<RequestPickup/>}/>
-                </Routes>
-            </div>
-
-        </Router>
+        </div>
     </>
     
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,11 @@
 import '../styles/Nav.css'
-// import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 // import { Home } from '../components/Home'
 // import { Contact } from '../components/Contact'
 // import { Error } from '../components/Error'
 // import { ServicesCard } from '../components/ServicesCard'
 import { BurgerMenu } from './BurgerMenu'
 import { useState } from 'react'
-
-// function FaqSection() {
-//   const [hasScrolled, setHasScrolled] = useState(false);
-//   const faqSectionRef = useRef<HTMLElement>(null);
-//   const { hash } = useLocation();
-
-//   useEffect(() => {
-//     if (faqSectionRef.current && hash === "#duvidas" && !hasScrolled) {
-//     faqSectionRef.current.scrollIntoView({ behavior: "smooth", inline:'center', block: "center",}) 
-//     setHasScrolled(true)
-//     }
-
-//     return () => {
-//       setHasScrolled(false)
-//     }
-
-//     }, [faqSectionRef, hash])
-
-//     return (
-
-//     <section id="duvidas" ref={faqSectionRef}>
-//       hi
-//     </section>
-
-//     )
-
-// }
-
 
 
 export const Navbar = () => {
@@ -52,37 +24,35 @@ export const Navbar = () => {
       </div>
       
 
-      {/* <Router> */}
+      <Router>
         
         {/* Nav Links */}
-        {/* <div className='nav-major-container'>
+        <div className='nav-major-container'>
 
           <div className='logo-container'>
             <p>Logo h</p>
           </div>
 
 
-
-          <nav className='nav-container'>
+          {/* <nav className='nav-container'>
             <Link to='/' className='home-link'>Home</Link>
             <Link to='/contact' className='contact-link'>Contact</Link>
             <Link to='/services' className='services-link'>Services</Link>
+          </nav> */}
 
-          </nav>
+        </div>
 
-        </div> */}
-
-        {/* <Routes> */}
-          {/* <Route path='/' element={<Home/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/services' element={<Services/>}/> */}
+        {/* <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contact' element={<Contact/>}/> */}
+          {/* <Route path='/services' element={<Services/>}/> */}
 
 
           {/* Error Route */}
-          {/* <Route path='*' element={<Error/>}/> */}
-        {/* </Routes> */}
+          {/* <Route path='*' element={<Error/>}/>
+        </Routes> */}
 
-      {/* </Router>  */}
+      </Router> 
 
       
     </>
