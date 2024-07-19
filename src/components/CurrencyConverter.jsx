4e37c826cbdd379e7ec9018a8e9d2e21d55a598f
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-07-19 14:53:52 +0000

    stuck, considering using useState()

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -7,9 +7,20 @@ const toField = document.querySelector('#to-field')
 const fromField = document.querySelector('#from-field')
 
 const selectFromEl = document.querySelector('.from-select')
-const fromUSD = document.querySelector('#USD')
+
+// from currencies
+const fromUSD = document.querySelector('#from-USD')
+const fromGHS = document.querySelector('#from-GHS')
+const fromEURO = document.querySelector('#from-EURO')
+const fromGBP = document.querySelector('#from-GBP')
 
 
+
+// to currencies
+const toUSD = document.querySelector('#to-USD')
+const toGHS = document.querySelector('#to-GHS')
+const toEURO = document.querySelector('#to-EURO')
+const toGBP = document.querySelector('#to-GBP')
 
 
 export const CurrencyConverter = () => {
@@ -19,17 +30,22 @@ export const CurrencyConverter = () => {
             const response = await fetch('https://api.exchangerate-api.com/v4/latest/ghs') // GHS is the base currency
             const parsedData = await response.json()
             
-            const API_RATES = parsedData['rates']
+            const API_RATES_OBJECT = parsedData['rates']
+            const currencies = Object.keys(API_RATES_OBJECT)
+            const currencyRates = Object.values(API_RATES_OBJECT)
+
             
-            // find from selected currency in drop down list
+            // GOAL: Converted the selected "from" currency to the "to" currency
+            // get "from" selected currency in drop down list
+            console.log(fromUSD)
 
-            // find to selected currency in drop down list
-
-            // 
+            // get "to" selected currency in drop down list
 
             
             // take number from from-field and multiply with eRate of target currency
             // set to-field value to the variable above
+
+
 
         } catch (error) {
             console.error(error)
@@ -59,10 +75,10 @@ export const CurrencyConverter = () => {
                     <div className="actual-converter">
                         <input type="number" className='number-field' id="from-field" />
                         <select name="" className="from-select" >
-                            <option value="USD" id="USD">USD</option>
-                            <option value="GHS" id="GHS">GHS</option>
-                            <option value="EUR" id="EUR">EUR</option>
-                            <option value="GBP" id="EUR">EUR</option>
+                            <option value="USD" id="from-USD">USD</option>
+                            <option value="GHS" id="from-GHS">GHS</option>
+                            <option value="EUR" id="from-EUR">EUR</option>
+                            <option value="GBP" id="from-GBP">GBP</option>
                         </select>
                     </div>
 
@@ -80,10 +96,10 @@ export const CurrencyConverter = () => {
                     <div className="actual-converter">
                         <input type="number" className='number-field' id="to-field" disabled/>
                         <select name="" className="to-select">
-                            <option value="USD" id="USD">USD</option>
-                            <option value="GHS" id="GHS">GHS</option>
-                            <option value="EUR" id="EUR">EUR</option>
-                            <option value="GBP" id="EUR">EUR</option>
+                            <option value="USD" id="to-USD">USD</option>
+                            <option value="GHS" id="to-GHS">GHS</option>
+                            <option value="EUR" id="to-EUR">EUR</option>
+                            <option value="GBP" id="to-GBP">GBP</option>
                         </select>
                     </div>
 
