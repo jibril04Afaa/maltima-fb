11f4d56a5fe8c79065d24e2757b3ae0b4466c042
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-07-19 14:15:43 +0000

    new api

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -3,31 +3,33 @@ import { RequestPickup } from "./RequestPickup.jsx"
 import { useState, useEffect } from "react"
 import { Router } from "react-router-dom"
 
-const toField = document.querySelector('.to-field')
-const fromField = document.querySelector('.from-field')
+const toField = document.querySelector('#to-field')
+const fromField = document.querySelector('#from-field')
 
 const selectFromEl = document.querySelector('.from-select')
 const fromUSD = document.querySelector('#USD')
+
+
 
 
 export const CurrencyConverter = () => {
     // API
     const convertCurrency = async() => {
         try {
-            const data = await fetch('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_clVUQQecPGeX7P34kbBEfvEjGjrdVUpXvRhsR86c')
-            const parsedData = await data.json()
+            const response = await fetch('https://api.exchangerate-api.com/v4/latest/ghs') // GHS is the base currency
+            const parsedData = await response.json()
+            
+            const API_RATES = parsedData['rates']
+            
+            // find from selected currency in drop down list
 
-            for (let eachCurrency in parsedData) {
-                // an object of conversion rates from USD to other currencies
-                let ratesObject = parsedData[eachCurrency]
-                // get the key of ratesObject
-                let keys = Object.keys(ratesObject)
-                let values = Object.values(ratesObject)
+            // find to selected currency in drop down list
 
-                // 1 - USD -> EUR
-                // take number from to-field and multiply with eRate of eur
+            // 
 
-            }
+            
+            // take number from from-field and multiply with eRate of target currency
+            // set to-field value to the variable above
 
         } catch (error) {
             console.error(error)
@@ -56,7 +58,7 @@ export const CurrencyConverter = () => {
 
                     <div className="actual-converter">
                         <input type="number" className='number-field' id="from-field" />
-                        <select name="" className="from-select" onChange={handleChange} >
+                        <select name="" className="from-select" >
                             <option value="USD" id="USD">USD</option>
                             <option value="GHS" id="GHS">GHS</option>
                             <option value="EUR" id="EUR">EUR</option>
@@ -66,6 +68,7 @@ export const CurrencyConverter = () => {
 
                     <div className="rate-container">
                         <div className="rate">
+                            {/* Exchange rate */}
                             <h6> 1 USD = 15.21 GHS</h6>
                         </div>
                     </div>
